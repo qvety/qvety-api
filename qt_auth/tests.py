290d56f61c5e0b8315d60b785ab6674da00a9a40
Kryuk <dan.hook.fibonacci@gmail.com>
2024-08-10 22:48:17 +0300

    [FRO-46] Add refresh tests

--- qt_auth/tests.py
+++ qt_auth/tests.py
@@ -1,8 +1,19 @@
+from datetime import datetime
+from unittest.mock import patch
+
 from django.test import TestCase
 
+from qt_auth.utils import create_access_token, create_refresh_token, get_verified_payload_from_token
 from qt_user.models import User
-from qt_auth.utils import get_verified_payload_from_token
-from datetime import datetime
+
+
+def _create_user(username, password, email):
+    return User.objects.create_user(
+        username=username,
+        password=password,
+        email=email,
+    )
+
 
 class SignUpTestCase(TestCase):
     def setUp(self):
@@ -82,18 +93,10 @@ class SignInTestCase(TestCase):
             'username': 'test',
             'email': 'test@test.com',
         }
-        self.user = self._create_user(
+        self.user = _create_user(
             username=self.base_user_data['username'],
             password=self.base_user_data['password'],
             email=self.base_user_data['email'],
-        )
-
-    @staticmethod
-    def _create_user(username, password, email):
-        return User.objects.create_user(
-            username=username,
-            password=password,
-            email=email,
         )
 
     def test_ok(self):
@@ -165,3 +168,96 @@ class SignInTestCase(TestCase):
         self.assertLess(fifteen_minutes_timestamp, verified_payload_refresh['exp'])
         self.assertEqual(verified_payload_refresh['user_id'], self.user.id)
         self.assertEqual(verified_payload_refresh['type'], 'refresh')
+
+
+class RefreshTokenTestCase(TestCase):
+    def setUp(self):
+        self.signin_url = '/api/auth/signin'
+        self.refresh_url = '/api/auth/refresh'
+        self.base_user_data = {
+            'password': 'superpass',
+            'username': 'test',
+            'email': 'test@test.com',
+        }
+        self.user = _create_user(
+            username=self.base_user_data['username'],
+            password=self.base_user_data['password'],
+            email=self.base_user_data['email'],
+        )
+        self.refresh = create_refresh_token(self.user)
+        self.access = create_access_token(self.user)
+
+    def test_ok(self):
+        user_data = {
+            'password': 'superpass',
+            'username': 'test',
+        }
+        resp_signin = self.client.post(self.signin_url, data=user_data, content_type='application/json')
+
+        data = resp_signin.json()
+        refresh_token = {'refresh': data['refresh']}
+
+        resp = self.client.post(self.refresh_url, data=refresh_token, content_type='application/json')
+        self.assertEqual(resp.status_code, 200)
+
+        data = resp.json()
+        self.assertIn('access', data)
+        self.assertIn('refresh', data)
+
+    def test_wrong_token(self):
+        refresh_token = {'refresh': 'zaluplka'}
+        resp = self.client.post(self.refresh_url, data=refresh_token, content_type='application/json')
+        self.assertEqual(resp.status_code, 401)
+
+        data = resp.json()
+        self.assertEqual(data['errors'][0]['jwt'], 'Token decode error')
+
+    def test_bad_token(self):
+        refresh_token = {
+            'refresh': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjU5MDI5NDIuMzIxNjU3LCJpYXQiOjE3MjMzMTA5NDIuMzIxNjU3LCJqdGkiOiJkMzI4N2JhNS03YWE2LTRmMzQtOWZiNC1hZGEzNGIzZWZkMWIiLCJ1c2VyX2lkIjoxLCJ0eXBlIjoicmVmcmVzaCJ9.u_JLiXcmbSWBiy7X64M9Sb0mqCZDH82_p4j5UgU7wEE'}  # noqa: E501
+        resp = self.client.post(self.refresh_url, data=refresh_token, content_type='application/json')
+        self.assertEqual(resp.status_code, 401)
+
+        data = resp.json()
+        self.assertEqual(data['errors'][0]['jwt'], 'Token decode error')
+
+    def test_miss_user_id_payload(self):
+        refresh_token = {
+            'refresh': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjU5MDI5NDIuMzIxNjU3LCJpYXQiOjE3MjMzMTA5NDIuMzIxNjU3LCJqdGkiOiJkMzI4N2JhNS03YWE2LTRmMzQtOWZiNC1hZGEzNGIzZWZkMWIiLCJ0eXBlIjoicmVmcmVzaCJ9.WdQdAV1hcGKX6PhE3PJIIZB4BlSOZLawxs6gBMZQInw'}  # noqa: E501
+        resp = self.client.post(self.refresh_url, data=refresh_token, content_type='application/json')
+        self.assertEqual(resp.status_code, 401)
+
+        data = resp.json()
+        self.assertEqual(data['errors'][0]['jwt'], 'User id not found in payload')
+
+    def test_wrong_user_id_payload(self):
+        refresh_token = {
+            'refresh': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjU5MDI5NDIuMzIxNjU3LCJpYXQiOjE3MjMzMTA5NDIuMzIxNjU3LCJqdGkiOiJkMzI4N2JhNS03YWE2LTRmMzQtOWZiNC1hZGEzNGIzZWZkMWIiLCJ1c2VyX2lkIjoyMjgsInR5cGUiOiJyZWZyZXNoIn0.m8KkJmuLeUx63qELDJKMNVPWjwH6pqaI9_kTkV0ogFE'}  # noqa: E501
+        resp = self.client.post(self.refresh_url, data=refresh_token, content_type='application/json')
+        self.assertEqual(resp.status_code, 401)
+
+        data = resp.json()
+        self.assertEqual(data['errors'][0]['user'], 'User dose not exist')
+
+    def test_token_type(self):
+        refresh_token = {
+            'refresh': self.access,
+        }
+        resp = self.client.post(self.refresh_url, data=refresh_token, content_type='application/json')
+        self.assertEqual(resp.status_code, 401)
+
+        data = resp.json()
+        self.assertEqual(data['errors'][0]['jwt'], 'Token type is not refresh')
+
+    def test_revoked_token(self):
+        with patch("qt_auth.api.is_token_revoked") as m_is_token_revoked:
+            m_is_token_revoked.return_value = True
+
+            refresh_token = {
+                'refresh': self.refresh,
+            }
+            resp = self.client.post(self.refresh_url, data=refresh_token, content_type='application/json')
+            self.assertEqual(resp.status_code, 401)
+
+            data = resp.json()
+            self.assertEqual(data['errors'][0]['jwt'], 'Refresh token has been revoked')
